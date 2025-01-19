013fec9ab71c96399da9a4f600f8c2e3b4071e15
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2025-01-19 22:51:24 +0530

    Update RecursivePower.cpp

--- RecursivePower.cpp
+++ RecursivePower.cpp
@@ -4,9 +4,11 @@ class Solution{
     int RecursivePower(int n,int p)
     {
         //Your code here
-        if( p == 0){
+        if( p == 0){       // Base case: anything raised to the power of 0 is 1
             return 1;
         }
+      // Recursive case: multiply n with the result of n^(p-1)
+
         return n * RecursivePower(n, p-1);
     }
 };
