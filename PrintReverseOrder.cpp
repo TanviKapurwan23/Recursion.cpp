e192d011ab61759d95a01dfc7d7838ea624dd2f5
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2025-01-19 22:22:48 +0530

    Create PrintReverseOrder.cpp

---
+++ PrintReverseOrder.cpp
@@ -0,0 +1,24 @@
+// C++ program to print all numbers between 1
+// to N in reverse order
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//Iterative function to print from N to 1
+void PrintReverseOrder(int N)
+{
+
+	for (int i = N; i > 0; i--)
+		cout << i << " ";
+
+}
+
+// Driven Code
+int main()
+{
+	int N = 5;
+
+	PrintReverseOrder(N);
+
+	return 0;
+}
