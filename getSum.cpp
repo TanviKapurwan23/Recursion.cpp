3750a8b11ea5efc6536467c0a813f3daf9137cc5
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2025-01-19 22:03:28 +0530

    Update getSum.cpp

--- getSum.cpp
+++ getSum.cpp
@@ -1,3 +1,4 @@
+// Recursive
 int getSum(int n){
     if(n == 0){
         return 0;
@@ -5,3 +6,20 @@ int getSum(int n){
         return getSum(n/10) + n%10;
     }
 }
+// O(d) :TC
+// O(d) : AS
+
+// Iterative
+
+int getSum(int n)
+{
+    int res = 0;
+    while(n>=0){
+    res = res + n%10;
+    n = n/10;
+     }
+    return res;
+}
+
+// TC:  O(d)
+// AS : O(1)
